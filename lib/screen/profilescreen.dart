a3ed314a00b95bca7cfe10daa057ec29d37fc3c7
lomanu4 <mapumah@inbox.ru>
2022-09-25 23:34:35 +0200

    profile screen

---
+++ lib/screen/profilescreen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key, required this.name,required this.email,required this.username});
+  final name;
+  final email;
+  final username;
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            appBar: AppBar(
+             
+              title: Text(
+                widget.name,// user username
+              ),
+              centerTitle: false,
+            ),
+            body: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            backgroundImage: NetworkImage(
+                              'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png', // user photo
+                            ),
+                            radius: 40,
+                          ),
+                      
+                        ],
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.only(
+                          top: 15,
+                        ),
+                        child: Text(
+                         widget.username,// user username
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.only(
+                          top: 1,
+                        ),
+                        child: Text(
+                         widget.email, // user bio
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+              
+              ],
+            ),
+          );
+  }
+}
--- lib/utils/global_varriable.dart
+++ lib/utils/global_varriable.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../models/user.dart';
 import '../screen/feedscreen.dart';
-
+import '../screen/profilescreen.dart';
+UsersTable user = UsersTable(id: 1, name: 'Nico', username: 'username', email: 'email', phone: 'phone', website: 'website');
 List<Widget> homeScreenitems = [
   
-  const FeedScreen()
+  const FeedScreen(),
+   ProfileScreen(
+    email: user.email,
+    name: user.name,
+    username: user.username,
+  ),
   ];
