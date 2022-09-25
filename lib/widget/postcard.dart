64eb42fde7fd2fbf31e223e97769334c4c2907cd
lomanu4 <mapumah@inbox.ru>
2022-09-25 22:27:25 +0200

    postcard update

--- lib/widget/postcard.dart
+++ lib/widget/postcard.dart
@@ -40,11 +40,64 @@ class _PostcardState extends State<Postcard> {
                             child: ClipOval(
                               child: Image.network('https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'),
                             ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                left: 10
+                              ),
+                              child:  Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                         
+                                              '',
+                                              // user name by post
+                                              style: const TextStyle(
+                                                fontFamily: 'Billabong',
+                                                fontSize: 20.0,
+                                              ),
+                                            ),
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Text(
+                                             '' ,// post title,
+                                              style: const TextStyle(
+                                                  fontFamily: 'Billabong',
+                                                  fontSize: 16.0,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                            const SizedBox(
+                                              height: 5,
+                                            ), // email
+                                  ],
+                                ),
+                              ),
+                            ),
                           )
                         ],
                       ),
                     ),
-                  )
+                  ),
+                  InkWell(
+                            onTap: () {
+                     // Postdetails
+                    },
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                               '',// post body
+                                //'A residenza sanitaria, in cui verrà prestato servizio, ospita anziani parzialmente e non autosufficienti. l`oss lavorerà su tre turni e si occuperà, in affiancamento al personale sanitario, di assistenza ad anziani parzialmente e non autosufficienti.in particolare si occuperà di- igiene personale, del letto e delle apparecchiature;- assistenza nell’alimentazione e nella vestizione;- aiuto nell’espletamento dei bisogni fisiologici;- posizionamento e mobilizzazioni del paziente.',
+                                style: const TextStyle(
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ),
+                          ),
                 ],
               ),
             ),
