6eeaa9197753895efd563a1c9c45ebb4f8ea9042
lomanu4 <mapumah@inbox.ru>
2022-09-25 23:11:48 +0200

    Multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'provider/comment_pprovider.dart';
+import 'provider/post_provider.dart';
+import 'provider/user_provider.dart';
 import 'reponsive/navigatorbarbutton.dart';
 
 void main() {
@@ -11,12 +14,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      //expose it to its descendants and rebuilds dependents whenever ChangeNotifier.notifyListeners is called
+      providers: [
+        ChangeNotifierProvider<GetAllUsers>(create: (_) => GetAllUsers()),
+        ChangeNotifierProvider<GetPostProvider>(create: (_) => GetPostProvider()),
+         ChangeNotifierProvider<GetCommentProvider>(create: (_) => GetCommentProvider()),
+      ],
+    child:
+    MaterialApp(
       title: 'Software Factory',
       theme: ThemeData(
         primaryColor: Colors.blue
       ),
       home: const NavButtonBar (),
-    );
+     ) );
   }
 }
