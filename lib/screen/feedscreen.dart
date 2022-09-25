e26ee4e659247ed7260ca8c0a52ef5d4698cab62
lomanu4 <mapumah@inbox.ru>
2022-09-25 23:30:10 +0200

    post details

--- lib/screen/feedscreen.dart
+++ lib/screen/feedscreen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
 
+import '../provider/comment_pprovider.dart';
+import '../provider/post_provider.dart';
+import '../provider/user_provider.dart';
 import '../widget/postcard.dart';
 
-class FeedScreen extends StatelessWidget {
+class FeedScreen extends StatefulWidget {
   const FeedScreen({super.key});
 
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  @override
+  @override
+  void initState() {
+    super.initState();
+    final provider = Provider.of<GetPostProvider>(context, listen: false);
+    provider.getMyData();
+    Provider.of<GetCommentProvider>(context, listen: false);
+    final userprovider = Provider.of<GetAllUsers>(context, listen: false);
+    userprovider.getUser();
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
--- lib/widget/postcard.dart
+++ lib/widget/postcard.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:softaware_factory/widget/postdetails.dart';
+import '../provider/post_provider.dart';
+import '../provider/user_provider.dart';
 
 class Postcard extends StatefulWidget {
   const Postcard({super.key});
@@ -12,99 +16,110 @@ class Postcard extends StatefulWidget {
 class _PostcardState extends State<Postcard> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return Column(
-          children: [
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 73, 122, 192).withOpacity(0.1),
-                borderRadius: BorderRadius.circular(25.0),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 3,
-                      horizontal: 10
-                    ).copyWith(right: 0),
-                    child: GestureDetector(
-                      onTap: (){
-                        // Profile Screeen
-                      },
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 18,
-                            child: ClipOval(
-                              child: Image.network('https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'),
-                            ),
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                left: 10
-                              ),
-                              child:  Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                         
-                                              '',
-                                              // user name by post
-                                              style: const TextStyle(
-                                                fontFamily: 'Billabong',
-                                                fontSize: 20.0,
-                                              ),
-                                            ),
-                                            const SizedBox(
-                                              height: 5,
-                                            ),
-                                            Text(
-                                             '' ,// post title,
-                                              style: const TextStyle(
-                                                  fontFamily: 'Billabong',
-                                                  fontSize: 16.0,
-                                                  fontWeight: FontWeight.w600),
-                                            ),
-                                            const SizedBox(
-                                              height: 5,
-                                            ), // email
-                                  ],
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
+    final userprovider = Provider.of<GetAllUsers>(context).users;
+    return Consumer<GetPostProvider>(builder: (context, value, child) {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 5.0),
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 73, 122, 192)
+                        .withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(25.0),
                   ),
-                  InkWell(
-                            onTap: () {
-                     // Postdetails
-                    },
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                               '',// post body
-                                //'A residenza sanitaria, in cui verrà prestato servizio, ospita anziani parzialmente e non autosufficienti. l`oss lavorerà su tre turni e si occuperà, in affiancamento al personale sanitario, di assistenza ad anziani parzialmente e non autosufficienti.in particolare si occuperà di- igiene personale, del letto e delle apparecchiature;- assistenza nell’alimentazione e nella vestizione;- aiuto nell’espletamento dei bisogni fisiologici;- posizionamento e mobilizzazioni del paziente.',
-                                style: const TextStyle(
-                                  color: Colors.black87,
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 3,
+                          horizontal: 10,
+                        ).copyWith(right: 0),
+                        child: GestureDetector(
+                          onTap: () {
+                            // Profile Screeen
+                          },
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 18,
+                                child: ClipOval(
+                                  child: Image.network(
+                                      'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'),
                                 ),
                               ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 10),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          userprovider[index].email,
+                                          // user name by post
+                                          style: const TextStyle(
+                                            fontFamily: 'Billabong',
+                                            fontSize: 20.0,
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          "${value.postModelClass[index].title}", // post title,
+                                          style: const TextStyle(
+                                              fontFamily: 'Billabong',
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                        const SizedBox(
+                                          height: 5,
+                                        ), // email
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                           Navigator.push(context, MaterialPageRoute(builder: (_) {
+                        return
+                          Postdetail(
+                            postid: value.postModelClass[index].id,
+                            post: value.postModelClass[index].body,
+                          );
+                            }));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "${value.postModelClass[index].body}", // post body
+                            //'A residenza sanitaria, in cui verrà prestato servizio, ospita anziani parzialmente e non autosufficienti. l`oss lavorerà su tre turni e si occuperà, in affiancamento al personale sanitario, di assistenza ad anziani parzialmente e non autosufficienti.in particolare si occuperà di- igiene personale, del letto e delle apparecchiature;- assistenza nell’alimentazione e nella vestizione;- aiuto nell’espletamento dei bisogni fisiologici;- posizionamento e mobilizzazioni del paziente.',
+                            style: const TextStyle(
+                              color: Colors.black87,
                             ),
                           ),
-                ],
-              ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            
-          ],
-        );
-      },
-    );
+          );
+        },
+      );
+    });
   }
 }
